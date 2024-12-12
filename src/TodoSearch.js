5f8bd42bace7749b1a06cc3bd07f31031531ec2c
Fidel Kneeteman <fidelkneeteman@gmail.com>
2024-12-12 17:27:42 -0300

    Se incorpora el uso de estados en react

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,8 +1,18 @@
-import "./TodoSearch.css"
+import React from 'react';
+import "./TodoSearch.css";
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('');
+
     return (
-        <input className= 'TodoSearch' placeholder="Ir al supermercado"/>
+        <input
+            className= 'TodoSearch' 
+            placeholder="Ir al supermercado"
+            value= {searchValue}
+            onChange={(event) => {
+                setSearchValue(event.target.value);
+            }}
+        />
     );
-  }
-  export { TodoSearch };
+}
+export { TodoSearch };
